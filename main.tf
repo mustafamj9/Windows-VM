75455d2be0af780613b0c5c055b4c10016e24741
mustafamj9 <mdmustafajaveed007@gmail>
2026-08-08 15:27:54 +0530

    upload

--- main.tf
+++ main.tf
@@ -16,8 +16,8 @@ resource "azurerm_public_ip" "public_ip" {
   name                = "PIP-IP"
   location            = azurerm_resource_group.RG.location
   resource_group_name = azurerm_resource_group.RG.name
-  sku                 = "Basic"
-  allocation_method   = "Dynamic"
+  sku                 = "Standard"
+  allocation_method   = "Static"
 }
 
 # create the NIC
